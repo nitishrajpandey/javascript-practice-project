4d5d0c2a95af9a902f94509cb55b1f24bb5c94ce
Nitish Raj <83996601+nitishrajpandey@users.noreply.github.com>
2023-09-18 05:55:13 +0000

    modified project 3

--- project 3/script.js
+++ project 3/script.js
@@ -9,12 +9,157 @@ function colorGenerator() {
     return color;
 }
 
+// ##################################################
+
+
+function colorGenerator2() {
+    const letter='1234567890ABCDEF';
+    let color='#';
+
+
+    for (let index = 0; index <6; index++) {
+        color+=letter[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+
+// ###############################################
+
+
+function colorGenerator3() {
+    const letter='1234567890ABCDEF';
+    let color='#';
+
+
+    for (let index = 0; index <6; index++) {
+        color+=letter[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+
+// ##########################################
+
+
+function colorGenerator4() {
+    const letter='1234567890ABCDEF';
+    let color='#';
+
+
+    for (let index = 0; index <6; index++) {
+        color+=letter[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+
+// #########################
+
+
+function colorGenerator5() {
+    const letter='1234567890ABCDEF';
+    let color='#';
+
+
+    for (let index = 0; index <6; index++) {
+        color+=letter[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+
+// ###############################
+
+
+function colorGenerator6() {
+    const letter='1234567890ABCDEF';
+    let color='#';
+
+
+    for (let index = 0; index <6; index++) {
+        color+=letter[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+
+
+
+// #######################
+
+
+
 function changeBackgroundColor() {
     const body=document.querySelector('[data-body]')
     const randomColor=colorGenerator();
     body.style.backgroundColor = randomColor
 }
 setInterval(changeBackgroundColor, 1000);
+
+// ######################################################
+
+
+function changeBackgroundColor2() {
+    const displayTime=document.querySelector('[data-displayTime]')
+    const randomColor=colorGenerator2();
+    // displayTime.style.backgroundColor = randomColor
+    // displayTime.style.color = randomColor
+    displayTime.style.borderColor = randomColor
+
+}
+setInterval(changeBackgroundColor2, 1000);
+
+// #####################################################
+
+function changeBackgroundColor3() {
+    const displayTime=document.querySelector('[data-displayTime]')
+    const randomColor=colorGenerator3();
+    // displayTime.style.backgroundColor = randomColor
+    displayTime.style.color = randomColor
+    // displayTime.style.borderColor = randomColor
+}
+setInterval(changeBackgroundColor3, 1000);
+
+
+
+// ######################################
+
+function changeBackgroundColor4() {
+    const displayTime=document.querySelector('[data-displayTime]')
+    const randomColor=colorGenerator4();
+    const hoursElement = document.getElementById('hours');
+    hoursElement.style.color=randomColor
+}
+setInterval(changeBackgroundColor4, 1000);
+
+
+// ##########################
+
+
+function changeBackgroundColor5() {
+    const displayTime=document.querySelector('[data-displayTime]')
+    const randomColor=colorGenerator5();
+    const secondsElement = document.getElementById('seconds');
+   
+    secondsElement.style.color=randomColor
+}
+setInterval(changeBackgroundColor5, 1000);
+
+// ############################
+
+
+function changeBackgroundColor6() {
+    const displayTime=document.querySelector('[data-displayTime]')
+    const randomColor=colorGenerator6();
+    const hoursElement = document.getElementById('hours');
+    hoursElement.style.color=randomColor
+}
+setInterval(changeBackgroundColor6, 1000);
+
+
+
+
+
 
 
 // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
