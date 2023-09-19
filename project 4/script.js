bab5ac91cd8e2ab3dff9cb282810bb6b46cfe957
Nitish Raj <83996601+nitishrajpandey@users.noreply.github.com>
2023-09-19 05:26:43 +0000

    guess game project complete

--- project 4/script.js
+++ project 4/script.js
@@ -1,9 +1,19 @@
 // create random Number between 1 to 100
-const Numbers = Math.floor(Math.random() * 100)
-console.log(Numbers);
+const Numbers = Math.floor(Math.random() * 100 +1)
+
+
 
 const previousGuesses = document.querySelector('[data-previousGuesses]');
+const previousGuessesText = document.querySelector('[data-previousGuessesText]');
 let previous = [];
+
+let noOfGuessLeft=10
+let noOfGuess=0
+
+let heading=document.querySelector('[data-heading]')
+let subHeading=document.querySelector('[data-subHeading]')
+
+const reset=document.querySelector('[data-resetButton]')
 
 
 
@@ -14,17 +24,81 @@ submitButton.addEventListener('click', function (e) {
 
     let inputValue = Number((document.querySelector('[data-inputValue]').value))
     let inputValue1 = document.querySelector('[data-inputValue]')
-    let result = document.querySelector('[data-result]')
+    
+    let guessesRemaining=document.querySelector('[data-guessesRemaining]')
+    let guessesRemainingText=document.querySelector('[data-guessesRemainingText]')
 
-    console.log(typeof inputValue);
+    let leftGuess=Number(guessesRemaining.textContent)
+
+    let hint=document.querySelector('[data-hint]')
+
+
+    
+
+
+
+    if (leftGuess==1) {
+        alert("game over")
+        inputValue1.disabled = true;
+        submitButton.disabled=true;
+    }
+
+
+    if (inputValue>0 && inputValue<=100) {
+        
+    
+
     if (inputValue === Numbers) {
 
         inputValue1.value=" "
-        result.textContent="scussfull"
+        previous.push(inputValue)
+        // previousGuesses.textContent=previous;
+        // noOfGuessLeft=noOfGuessLeft-1
+        // guessesRemaining.textContent=noOfGuessLeft
+        inputValue1.disabled = true;
+        submitButton.disabled=true;
+        inputValue1.style.display='none';
+        submitButton.style.display='none';
+        heading.textContent="congratulations 🎉👏"
+        reset.style.display='initial'
+        noOfGuess=noOfGuess+1
+        hint.style.display='none';
+        subHeading.style.display='block'
+        subHeading.innerHTML=`your previous guess : ${previous} <br/> you win in ${noOfGuess} attemps`
 
+        previousGuesses.style.display='none'
+        previousGuessesText.style.display='none'
+        guessesRemaining.style.display='none'
+        guessesRemainingText.style.display='none'
+
+
+
+
+        
     }
     else {
+        inputValue1.value=" "
         previous.push(inputValue)
         previousGuesses.textContent=previous;
+        noOfGuessLeft=noOfGuessLeft-1
+        noOfGuess=noOfGuess+1
+        guessesRemaining.textContent=noOfGuessLeft
+
+        if (inputValue>Numbers) {
+            hint.textContent=" Number is to high"
+        }
+        else if(inputValue<Numbers){
+            hint.textContent="number is to loow"
+        }
+        else if(inputValue===Numbers){
+            hint.style.display='none';
+        }
+        
     }
+}
+
+else{
+    alert("please enter between 1 to 100")
+}
+
 })
